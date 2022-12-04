962b438b414662302f6c96a5865f9f07810c822a
slykare <sylaninteriors@gmail.com>
2022-12-04 22:13:57 +0000

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    if my_list:
+        for value in my_list:
+            if value % 2 == 0:
+                new_list.append(True)
+            else:
+                new_list.append(False)
+    return(new_list)
