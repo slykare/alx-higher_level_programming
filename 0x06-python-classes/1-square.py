36efa5df796fb6e844e8b2ce17cf6901fa706e9e
slykare <sylaninteriors@gmail.com>
2022-12-20 17:03:35 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Define a class square
+"""
+
+
+class Square:
+    """
+    class Square that defines a square by: (based on 0-square.py)
+    """
+
+    def __init__(self, size):
+        """
+        Create instance of a Square
+        Args:
+            size: size of the square
+        """
+        self.__size = size
