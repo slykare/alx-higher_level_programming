13cc31b638edbb26cf28459519b4b7fbea0a5d6b
slykare <sylaninteriors@gmail.com>
2023-01-09 21:39:40 +0300

    my integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-'''
-100-my_int.py
-Inherits from int
-'''
+"""
+A class MyInt that inherits from int
+"""
 
 
 class MyInt(int):
-    '''
-    Class MyInt that inherits from int
-        MyInt is a rebel. MyInt has == and != operators inverted
-    '''
-    def __eq__(self, otehr):
-        '''
-        Method that returns != check
-        '''
-        return int.__ne__(self, otehr)
-    
-    def __ne__(self, otehr):
-        '''
-        Method that returns == check
-        '''
-        return int.__eq__(self, otehr)
+    """Module of a rebel"""
+    def __new__(cls, *args, **kwargs):
+        """creates object inside __new__ method by using super"""
+        return(super(MyInt, cls).__new__(cls, *args, **kwargs))
+
+    def __eq__(self, other):
+        """!= inverted to =="""
+        return(int(self) != other)
+
+    def __ne__(self, other):
+        """== inverted to !="""
+        return(int(self) == other)
