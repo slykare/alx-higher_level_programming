5f6d991f98a217d646638879699f5dc274232a50
slykare <sylaninteriors@gmail.com>
2023-01-09 21:22:53 +0300

    my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-'''
-1-my_list.py
-class MyList that inherits from list.
-'''
+"""
+A class MyList that inherits from list
+"""
 
 
 class MyList(list):
-    '''
-    class MyList that inherits from list
-    '''
+    """Derived class of super class list"""
+    def __init__(self):
+        """initializes the object's attributes"""
+        super().__init__()
+        """Calls the initialization method of the super class list"""
 
     def print_sorted(self):
-        '''
-        prints the list, but sorted (ascending sort)
-        '''
-
-        newlist = self[:]
-        newlist.sort()
-        print("{}".format(newlist))
+        """prints the sorted list (ascending sort)"""
+        print(sorted(self))
